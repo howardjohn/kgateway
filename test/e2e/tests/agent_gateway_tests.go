f114ec7a96488b75cc1d4666a33d90b3ce7819f1
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2025-11-12 12:39:21 -0800

    basic auth e2e agw tests
    Signed-off-by: Dmitri Dolguikh <dmitri@appliedlogic.ca>

--- test/e2e/tests/agent_gateway_tests.go
+++ test/e2e/tests/agent_gateway_tests.go
@@ -7,6 +7,7 @@ import (
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/a2a"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/aibackend"
+	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/basicauth"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/configmap"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/csrf"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/features/agentgateway/extauth"
@@ -32,6 +33,7 @@ func AgentgatewaySuiteRunner() e2e.SuiteRunner {
 	agentgatewaySuiteRunner.Register("BackendTLSPolicy", backendtls.NewAgentgatewayTestingSuite)
 	agentgatewaySuiteRunner.Register("AIBackend", aibackend.NewTestingSuite)
 	agentgatewaySuiteRunner.Register("ConfigMap", configmap.NewTestingSuite)
+	agentgatewaySuiteRunner.Register("BasicAuth", basicauth.NewTestingSuite)
 
 	return agentgatewaySuiteRunner
 }
